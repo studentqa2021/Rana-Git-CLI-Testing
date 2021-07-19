dd44bb8d82f1cf68d800b45f896f05f327412115
ABU RANA <sarowerhome@gmail.com>
2021-07-18 21:30:50 -0400

    Added animal class

---
+++ src/test/java/com/practice/Animal.java
@@ -0,0 +1,17 @@
+package com.practice;
+
+public class Animal {
+
+	private int a=10; //global variable
+
+	
+	public int getA() {
+		return a;
+	}
+
+	public void setA(int a) {
+		this.a = a; //local variable
+	}
+	
+	
+}
